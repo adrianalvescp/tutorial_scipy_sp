9131a8ada9bee089e908b877c7ee73f64cb1dec4
Adriana Alves <adrianalves.cp@gmail.com>
2016-10-21 19:53:02 -0200

    Primeira versao

---
+++ lista_encontros.py
@@ -0,0 +1,9 @@
+# lista_encontros.py 
+def encontro_scipy_sp(id=4, date="21/10/2016", presenter="Mauricio Vieira"):
+    print("SciPy-SP #{id}: {date}, apresentado por {presenter}".format(
+        id=id, 
+        date=date, 
+        presenter=presenter
+        )
+    )
+
